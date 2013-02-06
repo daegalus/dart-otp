b8cc825469d15a0c3c862f982111d2e7b4515b71
Yulian Kuncheff <yulian@kuncheff.com>
2013-02-05 20:04:54 -0800

    Fixes for future breaking changes.

--- lib/OTP.dart
+++ lib/OTP.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'dart:crypto';
 
 main() {
-  print(OTP.generateTOTPCode("JBSWY3DPEHPK3PXP", (new Date.now()).toUtc().millisecondsSinceEpoch));
+  print(OTP.generateTOTPCode("JBSWY3DPEHPK3PXP", (new DateTime.now()).toUtc().millisecondsSinceEpoch));
 }
 
 class OTP {
@@ -11,50 +11,50 @@ class OTP {
     time = (((time ~/ 1000).round())~/30).floor();
     return _generateCode(secret, time, length);
   }
-  
+
   static int generateHOTPCode(String secret, int counter, {int length: 6}) {
     return _generateCode(secret, counter, length);
   }
-  
+
   static int _generateCode(String secret, int time, int length){
     length = (length <=8 && length > 0) ? length : 6;
-    
-    
+
+
     var secretList = base32.decode(secret);
     var timebytes = _int2bytes(time);
-    
+
     var hmac = new HMAC(new SHA1(), secretList);
     hmac.update(timebytes);
     var hash = hmac.digest();
- 
+
     int offset = hash[hash.length - 1] & 0xf;
 
     int binary = ((hash[offset] & 0x7f) << 24) |
                  ((hash[offset + 1] & 0xff) << 16) |
                  ((hash[offset + 2] & 0xff) << 8) |
                  (hash[offset + 3] & 0xff);
-    
+
     print(binary);
 
     return binary % pow(10,length);
   }
-  
+
   static String randomSecret() {
     var rand = new Random();
     var bytes = new List();
-    
+
     for(int i = 0; i < 10; i++) {
       bytes.add(rand.nextInt(256));
     }
-    
+
     return base32.encode(bytes);
   }
-  
-  static String _dec2hex(int s) { 
+
+  static String _dec2hex(int s) {
     var st = s.round().toRadixString(16);
-    return (st.length % 2 == 0) ? st : '0'.concat(st); 
-  } 
-  
+    return (st.length % 2 == 0) ? st : '0'.concat(st);
+  }
+
   static String _leftpad(String str, int len, String pad) {
     var padded = '';
     for(int i = str.length; i < len; i++) {
@@ -62,7 +62,7 @@ class OTP {
     }
     return padded.concat(str);
   }
-  
+
   static List _hex2bytes(hex) {
     List bytes = new List(hex.length~/2);
     for(int i=0; i < hex.length; i+=2) {
@@ -72,7 +72,7 @@ class OTP {
     }
     return bytes;
   }
-  
+
   static List _int2bytes(int long) {
     // we want to represent the input as a 8-bytes array
     var byteArray = [0, 0, 0, 0, 0, 0, 0, 0];
